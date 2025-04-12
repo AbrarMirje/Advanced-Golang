63a10dd864271d71028a9fb7c78e00d5d60c8c57
Abrar Mirje <abrarmirje5@gmail.com>
2025-04-12 17:59:20 +0530

    Formatting Verbs

---
+++ 05-FormattingVerbs/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	i := 1_5.5
+	fmt.Printf("%v\n", i)
+	fmt.Printf("%#v\n", i)
+	fmt.Printf("%v%%\n", i)
+
+	// Formatting Verbs
+	// https://www.w3schools.com/go/go_formatting_verbs.php
+}
