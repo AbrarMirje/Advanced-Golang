98f022bc34b1e8cee763cd711c83946d9dfa4725
Abrar Mirje <abrarmirje5@gmail.com>
2025-04-18 11:15:42 +0530

    String Builder

---
+++ 15-String-Builder/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	// Create string builder
+	var builder strings.Builder
+
+	// Write some string
+	builder.WriteString("Hello")
+	builder.WriteString(", ")
+	builder.WriteString("World")
+
+	// Convert builder to string
+	result := builder.String()
+	fmt.Println(result)
+
+	// Using WriteRune to add a character
+	builder.WriteRune(' ')
+	len, _ := builder.WriteString("How are you")
+	fmt.Println("Length:", len)
+
+	result = builder.String()
+	fmt.Println(result)
+
+	// Reset the builder
+	builder.Reset()
+	builder.WriteString("New Fresh String")
+	result = builder.String()
+	fmt.Println(result)
+}
